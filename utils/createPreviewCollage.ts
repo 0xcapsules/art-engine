995e46b1c73ee979188adf9cb874129afbbbfa46
GeorgeSte <stefosgiwrgos@gmail.com>
2023-01-21 14:26:27 +0200

    Refactor createPreviewCollage to run as .ts

--- utils/createPreviewCollage.ts
+++ utils/createPreviewCollage.ts
@@ -1,26 +1,17 @@
 "use strict";
 
-// @ts-expect-error TS(2451): Cannot redeclare block-scoped variable 'isLocal'.
-const isLocal = typeof process.pkg === "undefined";
-// @ts-expect-error TS(2451): Cannot redeclare block-scoped variable 'basePath'.
-const basePath = isLocal ? process.cwd() : path.dirname(process.execPath);
-// @ts-expect-error TS(2451): Cannot redeclare block-scoped variable 'fs'.
-const fs = require("fs");
-// @ts-expect-error TS(2451): Cannot redeclare block-scoped variable 'path'.
-const path = require("path");
-// @ts-expect-error TS(2451): Cannot redeclare block-scoped variable 'createCanv... Remove this comment to see the full error message
-const { createCanvas, loadImage } = require("canvas");
-// @ts-expect-error TS(2451): Cannot redeclare block-scoped variable 'buildDir'.
-const buildDir = `${basePath}/build`;
-
-// @ts-expect-error TS(2451): Cannot redeclare block-scoped variable 'preview'.
-const { preview } = require(path.join(basePath, "/src/config.js"));
+//const isLocal = typeof process.pkg === "undefined";
+// const basePath = isLocal ? process.cwd() : path.dirname(process.execPath);
+const basePath = process.cwd();
+import fs from "fs";
+import path from "path";
+import  { createCanvas, loadImage } from "canvas";
+const buildDir: string = `${basePath}/build`;
+import { preview } from "../src/config";
 
 // read json data
-// @ts-expect-error TS(2451): Cannot redeclare block-scoped variable 'rawdata'.
-const rawdata = fs.readFileSync(`${basePath}/build/json/_metadata.json`);
-// @ts-expect-error TS(2451): Cannot redeclare block-scoped variable 'metadataLi... Remove this comment to see the full error message
-const metadataList = JSON.parse(rawdata);
+const rawdata: Buffer = fs.readFileSync(`${basePath}/build/json/_metadata.json`);
+const metadataList = JSON.parse(rawdata.toString());
 
 const saveProjectPreviewImage = async (_data: any) => {
   // Extract from preview config
